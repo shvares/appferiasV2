94a3d5765bb52326e1982c2579fec047a58ca093
rony rodas <rerc100xcientobarsa@gmail.com>
2019-11-12 12:27:07 -0600

    mas elementos con el map

--- app/src/main/java/com/asociacion/ferias/MainActivity.java
+++ app/src/main/java/com/asociacion/ferias/MainActivity.java
@@ -2,13 +2,50 @@ package com.asociacion.ferias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.asociacion.ferias.conciertos.Lista_Conciertos;
+import com.asociacion.ferias.cultura.Lista_Cultura;
+import com.asociacion.ferias.patrocinadores.Lista_Patrocinadores;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btncultura, btnconciertos, btnpatrocinadores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btncultura = (Button) findViewById(R.id.btncultura);
+        btnconciertos = (Button) findViewById(R.id.btnconciertos);
+        btnpatrocinadores = (Button) findViewById(R.id.btnpatrocinadores);
+
+        btncultura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Lista_Cultura.class);
+                startActivity(intent);
+            }
+        });
+
+        btnconciertos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Lista_Conciertos.class);
+                startActivity(intent);
+            }
+        });
+
+        btnpatrocinadores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Lista_Patrocinadores.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
